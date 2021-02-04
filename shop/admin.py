4c9bb75cd4f2dedba9c1e1a39ec5516d8fb59c98
Laurent <laurentk67@hotmail.fr>
2021-02-04 22:23:46 +0100

    ok

--- shop/admin.py
+++ shop/admin.py
@@ -11,6 +11,6 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'price', 'available', 'created', 'updated')
-    list_filter = ['available', 'created', 'update']
+    list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
